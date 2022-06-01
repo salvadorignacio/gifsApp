80dea7790267a7f1228e969c62d4c6a8f2eb03a4
salvadorignacio <salvadorignacio23@gmail.com>
2022-05-31 20:45:12 -0400

    Creación de interface GIF, más aplicación de busquedas de GIFS

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,32 @@ export class GifsService {
 
   private _historial: string[] = [];
   private apiKey    : string = "QR3DcfIYIqhlPEmmbJ7I8B5Am3XKPIvx";
+
+  public resultados: Gif [] = [];
+
   get historial() {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient){}
+  constructor( private http: HttpClient){
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+
+  }
 
   buscarGifs ( query: string) {
     query = query.trim().toLowerCase();
     if(!this._historial.includes(query)){
       this._historial.unshift(query);   
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial))
     }
     console.log(this._historial);
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=QR3DcfIYIqhlPEmmbJ7I8B5Am3XKPIvx&q=jujutsu')
-      .subscribe((resp : any) => {console.log(resp.data);
-      
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=QR3DcfIYIqhlPEmmbJ7I8B5Am3XKPIvx&q=${query}`)
+      .subscribe((resp ) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
       });
   }
 }
